c294fc589d4a1c9d9c4dc8ca492cbcac5d024457
carol-x <carolx@berkeley.edu>
2023-04-29 11:07:14 -0400

    structuring inputs and sbox setup

--- poseidon/src/poseidon2.rs
+++ poseidon/src/poseidon2.rs
@@ -6,3 +6,7 @@ use rand::distributions::Standard;
 use rand::prelude::Distribution;
 use rand::Rng;
 
+// reference Poseidon2 paper: https://eprint.iacr.org/2023/323.pdf
+// reference Poseidon2 code: https://github.com/HorizenLabs/poseidon2/blob/main/plain_implementations/src/poseidon2/poseidon2.rs
+
+/// The Poseidon permutation 
